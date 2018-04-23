684a2521745c248789fe91623d7aea406baf28c1
Watanabe <dixie@mw2.f837>
2018-04-24 04:18:31 +0900

    color test

--- terminal-256color.sh
+++ terminal-256color.sh
@@ -1,5 +1,5 @@
-for i in {255..0} ; do
-    printf "\x1b[38;5;${i}mcolour${i} "
+for i in `seq 255 -1 0`; do
+    echo -en "\e[38;5;${i}m${i}\e[0m "
 done
 printf "\n"
-
+for i in {255..0} ; do echo -en "\e[38;5;${i}m${i}\e[0m " ; done ; echo
