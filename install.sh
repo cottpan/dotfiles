e703be29cb16f4c4f36a5ecccd08a07483bad5b2
cottpan <lok.mem@gmail.com>
2023-02-06 20:17:44 +0900

    fix if

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ DOTPATH=$HOME/dotfiles
 # アーキテクチャ名は UNAME に入れておく
 UNAME=`uname -m`
 
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$(uname)" == "Darwin" ]]; then
 	echo "macOS detected. "
 else 
 	echo "Not macOS!"
@@ -16,18 +16,18 @@ else
 fi
 
 # Rosetta2 でターミナルを動かしている時には強制終了させる
-if [ "$UNAME-$(arch -arm64 uname -m)" == "x86_64-arm64" ]; then
+if [[ "$UNAME-$(arch -arm64 uname -m)" == "x86_64-arm64" ]]; then
 	echo "This script can not exec in Rosetta2 terminal"
 	exit 1
 fi
 
-if ![ xcode-select -p > /dev/null 2>&1 ]; then
+if ![[ xcode-select -p > /dev/null 2>&1 ]]; then
   # Install homebrew in Intel Mac or M1 Mac on Rosetta2
   echo "Installing Xcode CLT..."
   xcode-select --install
 fi
 
-if [ ! -d ~/dotfiles ]; then
+if [[ ! -d ~/dotfiles ]]; then
   cd ~
   echo "Cloning dotfiles..."
   git clone https://github.com/cottpan/dotfiles.git
