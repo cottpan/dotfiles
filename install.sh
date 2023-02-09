8cab610526eb610a37b14136b91ca01f57202141
cottpan <lok.mem@gmail.com>
2023-02-09 11:15:09 +0900

    fix

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ DOTFILES_GITHUB="https://github.com/cottpan/dotfiles.git"; export DOTFILES_GITHU
 UNAME=`uname -m`
 
 is_macos() {
-	test "$(uname)" == "Darwin"
+    test "$(uname)" == "Darwin"
 }
 
 is_arm() { 
@@ -20,17 +20,17 @@ is_arm() {
 }
 
 is_rosseta2() {
-	test "$UNAME-$(arch -arm64 uname -m)" == "x86_64-arm64"
+    test "$UNAME-$(arch -arm64 uname -m)" == "x86_64-arm64"
 }
 
 dotfiles_download() {
     if [ -d "$DOTPATH" ]; then
         echo "error: $DOTPATH: already exists"
-	elif [ -n "$CI" ] ; then
-		echo "Working on CI"
-	else
-		echo "Downloading dotfiles..."
-    	git clone --recursive "$DOTFILES_GITHUB" "$DOTPATH"
+    elif [ -n "$CI" ] ; then
+        echo "Working on CI"
+    else
+        echo "Downloading dotfiles..."
+        git clone --recursive "$DOTFILES_GITHUB" "$DOTPATH"
     fi
 }
 
@@ -39,29 +39,29 @@ is_clt_installed() {
 }
 
 if [ -n "$CI" ] ; then
-	DOTPATH=$RUNNER_WORKSPACE/dotfiles
+    DOTPATH=$RUNNER_WORKSPACE/dotfiles
 fi
 
 if ! is_macos ; then
-	echo "not macOS! Abort."
-	exit 1
+    echo "not macOS! Abort."
+    exit 1
 fi
 
 # Rosetta2 でターミナルを動かしている時には強制終了させる
 if ! is_arm ; then
-	echo "x86 Processor Detected"
-	if is_rosseta2 ; then
-		echo "This script can not exec in Rosetta2 terminal. Abort."
-		exit 1
-	fi
+    echo "x86 Processor Detected"
+    if is_rosseta2 ; then
+        echo "This script can not exec in Rosetta2 terminal. Abort."
+        exit 1
+    fi
 else
-	echo "ARM Processor Detected."
+    echo "ARM Processor Detected."
 fi
 
 if !( xcode-select -p > /dev/null 2>&1 ); then
-  echo "Installing Xcode CLT..."
-  echo "Please re-run after Xcode CLT installation is complete."
-  xcode-select --install
+    echo "Installing Xcode CLT..."
+    echo "Please re-run after Xcode CLT installation is complete."
+    xcode-select --install
 fi
 
 dotfiles_download
